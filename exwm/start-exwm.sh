250c97a11848a168032e19d52d2416037fc6e97c
Sebastian Munera <sfmunera@gmail.com>
2023-08-15 18:22:03 -0700

    Add EXWM configuration

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Set the screen DPI
+xrdb ~/.emacs.d/exwm/Xresources
+
+compton &
+
+# Enable screen locking on suspend
+xss-lock -- slock &
+
+# start EXWM session
 exec dbus-launch --exit-with-session emacs -mm --debug-init
