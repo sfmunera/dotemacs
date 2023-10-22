3ccfafb5578a8983ebd4f5f2fd6c2d57f4002dc2
Sebastian Munera <sfmunera@gmail.com>
2023-10-22 12:34:23 -0700

    Configure tree-sitter

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
 ;; Default coding system
 (set-default-coding-systems 'utf-8)
 
-(setq user-emacs-directory (expand-file-name "~/.cache/emacs"))
+(setq user-emacs-directory (expand-file-name "~/.emacs.d"))
 
 ;; Remap listing buffers to ibuffer
 (global-set-key [remap list-buffers] 'ibuffer); C-x C-b
@@ -835,6 +835,37 @@
 (defun sm/lsp-mode-setup ()
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
   (lsp-headerline-breadcrumb-mode))
+
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+     (java "https://github.com/tree-sitter/tree-sitter-java")
+     (rust "https://github.com/tree-sitter/tree-sitter-rust")))
+
+(setq major-mode-remap-alist
+ '((yaml-mode . yaml-ts-mode)
+   (bash-mode . bash-ts-mode)
+   (js2-mode . js-ts-mode)
+   (typescript-mode . typescript-ts-mode)
+   (json-mode . json-ts-mode)
+   (css-mode . css-ts-mode)
+   (elisp-mode . elisp-ts-mode)
+   (java-mode . java-ts-mode)
+   (rust-mode . rust-ts-mode)
+   (python-mode . python-ts-mode)))
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
