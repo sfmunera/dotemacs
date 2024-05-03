87b30224eb9130f2003b66ce3a6e96c1bd6abfd4
Sebastian Munera <smmunera@amazon.com>
2024-05-03 16:06:08 -0700

    Configure activities.el

--- init.el
+++ init.el
@@ -408,6 +408,23 @@
   ("s-]" . tab-bar-move-tab)
   ("s-[" . tab-bar-select-tab))
 
+(use-package activities
+  :init
+  (activities-mode)
+  (activities-tabs-mode)
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
+
+  :bind
+  (("C-x C-a C-n" . activities-new)
+   ("C-x C-a C-d" . activities-define)
+   ("C-x C-a C-a" . activities-resume)
+   ("C-x C-a C-s" . activities-suspend)
+   ("C-x C-a C-k" . activities-kill)
+   ("C-x C-a RET" . activities-switch)
+   ("C-x C-a b" . activities-switch-buffer)
+   ("C-x C-a g" . activities-revert)
+   ("C-x C-a l" . activities-list)))
 
 ;;; Completions
 
