1c51e131af9ca5c7f3a39944a3e09b46fe394507
Sebastian Munera <smmunera@amazon.com>
2024-05-25 22:30:26 -0700

    Modify keybindings for custom push/pop mark commands

--- init.el
+++ init.el
@@ -62,14 +62,14 @@
   (interactive)
   (push-mark (point) t nil)
   (message "Pushed mark to ring"))
-(global-set-key (kbd "C-`") 'my/push-mark-no-activate)
+(global-set-key (kbd "C-'") 'my/push-mark-no-activate)
 
 (defun my/jump-to-mark ()
   "Jumps to the local mark, respecting the `mark-ring' order.
   This is the same as using \\[set-mark-command] with the prefix argument."
   (interactive)
   (set-mark-command 1))
-(global-set-key (kbd "M-`") 'my/jump-to-mark)
+(global-set-key (kbd "M-'") 'my/jump-to-mark)
 
 ;;;; Package manager
 (setq package-enable-at-startup nil)
