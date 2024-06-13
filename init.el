c7099854cc3d2efd0d8760f109c6de86f76bfc80
Sebastian Munera <smmunera@amazon.com>
2024-06-13 11:46:08 -0700

    Adjust font size

--- init.el
+++ init.el
@@ -198,9 +198,9 @@
 ;;; Look and feel
 
 (defun my/set-font-faces ()
-  (set-face-attribute 'default nil :family "Iosevka Comfy" :height 160)
-  (set-face-attribute 'fixed-pitch nil :family "Iosevka Comfy" :height 160)
-  (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy Motion Duo" :height 160 :weight 'regular))
+  (set-face-attribute 'default nil :family "Iosevka Comfy" :height 145)
+  (set-face-attribute 'fixed-pitch nil :family "Iosevka Comfy" :height 145)
+  (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy Motion Duo" :height 145 :weight 'regular))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
