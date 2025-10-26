e30dbdfaf9d6fef9c688c9b3c33a1213bb50d184
Sebastian Munera <sfmunera@gmail.com>
2025-10-26 12:52:48 -0700

    Make TRAMP `sudo' function a bit safer

--- lisp/my-tramp.el
+++ lisp/my-tramp.el
@@ -9,13 +9,21 @@
 
 ;; Set default connection mode to SSH
 (setq tramp-default-method "ssh")
+
 (defun sudo ()
-  "Use TRAMP to `sudo' the current buffer."
+  "Use TRAMP to `sudo' the current buffer.
+Prompts to save if buffer is modified."
   (interactive)
-  (when buffer-file-name
+  (unless buffer-file-name
+    (user-error "Buffer is not visiting a file"))
+  (when (and (buffer-modified-p)
+             (not (y-or-n-p "Buffer modified. Save before sudo? ")))
+    (user-error "Aborted"))
+  (when (buffer-modified-p)
+    (save-buffer))
+  (let ((file buffer-file-name))
     (find-alternate-file
-     (concat "/sudo:root@localhost:"
-             buffer-file-name))))
+     (concat "/sudo:root@localhost:" file))))
 
 (provide 'my-tramp)
 ;;; my-tramp.el ends here
