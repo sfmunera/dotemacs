9f3ea38fb9c9163a9e1a102ba4d1359707bd0ad9
Sebastian Munera <sfmunera@gmail.com>
2025-10-26 13:03:44 -0700

    Remove unused backup directory

--- lisp/my-core.el
+++ lisp/my-core.el
@@ -71,7 +71,6 @@
 
 (use-package no-littering)
 
-(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
 ;; auto-save-mode doesn't create the path automatically
 (make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
 (setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
